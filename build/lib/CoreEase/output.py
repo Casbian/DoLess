dc8f9dacf68565a534a233c7cb29bfa46b2c17f2
Casbian <kai.erichreineke@gmail.com>
2025-01-18 18:39:45 +0100

    Pypy Update

--- build/lib/CoreEase/output.py
+++ build/lib/CoreEase/output.py
@@ -1,5 +1,5 @@
-import shutil
 import os
+import system
 def ClearConsole():
     os.system("cls")
 def Print(string):
@@ -10,17 +10,14 @@ def EnumeratedPrint(list):
     for x,y in enumerate(list):
         print(x + y)
 def CenteredPrint(string,filler):
-    console_w = shutil.get_terminal_size().columns
-    console_h = shutil.get_terminal_size().lines
+    console_w = system.ConsoleWidth()
     print(string.center(console_w, filler))
 def CenteredInput(string):
-    console_w = shutil.get_terminal_size().columns
-    console_h = shutil.get_terminal_size().lines
+    console_w = system.ConsoleWidth()
     console_lp = console_w // 2 - (len(string)+2)
     x = input(" " * console_lp + string + " " * 5)
     return x
 def EnumeratedCenteredPrint(list):
-    console_w = shutil.get_terminal_size().columns
-    console_h = shutil.get_terminal_size().lines
+    console_w = system.ConsoleWidth()
     for x,y in enumerate(list):
         print(x + y.center(console_w, " "))
