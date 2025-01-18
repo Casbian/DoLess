15d7cec3473ceb20d5b31a36ca96f70564ccbe43
Casbian <kai.erichreineke@gmail.com>
2025-01-18 12:09:56 +0100

    __init__.py Changes

--- DoLess/__init__.py
+++ DoLess/__init__.py
@@ -1,26 +1,4 @@
-from .executor import SubmitTasktoThreadWITHLoadBuffer
-from .executor import SubmitTasktoThreadNOLoadBuffer
-from .executor import LoadBuffer
-
-from .explorer import CheckFileExistence
-from .explorer import CreateFile
-from .explorer import ReadFile
-from .explorer import AppendtoFile
-from .explorer import OverwriteFile
-
-from .webdriver import StartBrowser
-from .webdriver import ClickButton
-from .webdriver import GoToUrl
-from .webdriver import OnlinePortalLoginTry
-from .webdriver import DetectChangeinWebsite
-from .webdriver import ParseforLoginEntrysandSubmitButton
-from .webdriver import ParseforLinks
-from .webdriver import ParseforButtons
-
-from .output import ClearConsole
-from .output import Print
-from .output import Input
-from .output import EnumeratedPrint
-from .output import CenteredPrint
-from .output import CenteredInput
-from .output import EnumeratedCenteredPrint
+from . import executor
+from . import explorer
+from . import webdriver
+from . import output
