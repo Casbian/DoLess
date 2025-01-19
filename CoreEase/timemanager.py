730835f5c3fce6aa1e7aaf7b7f3198af638d6677
Casbian <kai.erichreineke@gmail.com>
2025-01-19 18:10:51 +0100

    TimeManager Error Feedback2

--- CoreEase/timemanager.py
+++ CoreEase/timemanager.py
@@ -4,8 +4,8 @@ def CalculateTimeDifferencetoTargetTime24HourFrame(targettime):
     currenttime = CurrentSystemTime()
     try:
         targettime = datetime.datetime.combine(currenttime.date(), datetime.datetime.strptime(targettime, "%H:%M").time())
-    except ValueError:
-        return "Error try Format:  %H:%M"
+    except (ValueError, TypeError):
+        return "Error try Format: str(%H:%M)"
     timedifference = targettime - currenttime
     if timedifference.total_seconds() <= 0:
         return False
